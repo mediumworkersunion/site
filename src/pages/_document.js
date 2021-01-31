9e6d7262ed980d2005adfadb1e1e7ee9c397c1af
therebelrobot <github@therebelrobot.com>
2021-01-30 20:01:58 -0800

    removed conflicting index and updated document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,20 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta charset="utf-8" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <meta
+            name="description"
+            content="As the workers at Medium, we want Actions to Matter too"
+          />
+          <link rel="apple-touch-icon" href="/logo192.png" />
+          <meta name="robots" content="noindex" />
+          <link rel="manifest" href="/manifest.json" />
+          <title>MWU</title>
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
