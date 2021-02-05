7a1d33f2a2bb641c3f516482a1639ee57f2e1f58
therebelrobot <github@therebelrobot.com>
2021-02-05 15:39:21 -0800

    restructured homepage to match content

--- src/components/layout/partials/Logo.js
+++ src/components/layout/partials/Logo.js
@@ -1,41 +1,58 @@
-import React from 'react';
-import classNames from 'classnames';
-import Link from 'next/link';
-import Image from '../../elements/Image';
+import classNames from "classnames";
+import Link from "next/link";
+import React from "react";
 
+import { useContentful } from "../../../contentful/useContentful";
+import Image from "../../elements/Image";
 
-import { useContentful } from '../../../contentful/useContentful';
+const Logo = ({ className, ...props }) => {
+  const { homepageData: data } = useContentful();
 
-const Logo = ({
-  className,
-  ...props
-}) => {
-
-  const {homepageData: data} = useContentful()
-
-  const classes = classNames(
-    'brand',
-    className
-  );
+  const classes = classNames("brand", className);
 
   return (
-    <div
-      {...props}
-      className={classes}
-    >
+    <div {...props} className={classes}>
       <h1 className="m-0">
-        <Link href="/"><div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
-          <Image
-            src={data['Logo'].media.url}
-            alt={data['Logo'].text}
-            width={32}
-            height={32} />
-          <span className="text-m" style={{marginLeft: '8px', position: 'relative', top:3, fontFamily: 'BebasNeue', fontWeight: 400}}>{data['Name'].text}</span>
+        <Link href="/">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              src={data["Logo"].media.url}
+              alt={data["Logo"].text}
+              width={32}
+              height={32}
+            />
+
+            {data["Name"].text.split(" ").map((word, i) => {
+              return (
+                <>
+                  <span
+                    className="text-h3"
+                    style={{
+                      marginLeft: "8px",
+                      position: "relative",
+                      top: 3,
+                      fontFamily: "BebasNeue",
+                      fontSize: "32px",
+                      fontWeight: 400,
+                      color: i === 1 ? "#ff6161" : "white",
+                    }}
+                  >
+                    {word}
+                  </span>{" "}
+                </>
+              );
+            })}
           </div>
         </Link>
       </h1>
     </div>
   );
-}
+};
 
 export default Logo;
