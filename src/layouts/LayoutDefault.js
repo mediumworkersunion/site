751363df5a031c8fec2f69d1420afec90a20e1f5
therebelrobot <github@therebelrobot.com>
2021-02-08 09:12:40 -0800

    added GA tracking

--- src/layouts/LayoutDefault.js
+++ src/layouts/LayoutDefault.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import Header from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
+import {initGA, logPageView} from '../utils/analytics'
 
-const LayoutDefault = ({ children }) => (
-  <>
+const LayoutDefault = ({ children }) => {
+  React.useLayoutEffect(()=> {
+    console.log(process.env.NODE_ENV, process.browser)
+    if (process.env.NODE_ENV !== "production" ||!process.browser) return
+    console.log('should init')
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+    console.log('should log')
+    logPageView()
+  }, [process.browser])
+  return (<>
     <Header navPosition="right" className="reveal-from-bottom" />
     <main className="site-content">
       {children}
@@ -11,5 +23,6 @@ const LayoutDefault = ({ children }) => (
     <Footer />
   </>
 );
+}
 
 export default LayoutDefault;  
