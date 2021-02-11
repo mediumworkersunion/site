e0a02e8b0947f9bfbec4749987a6e6d2d1cc9b7f
therebelrobot <github@therebelrobot.com>
2021-02-11 11:02:52 -0800

    updated head tags

--- src/App.js
+++ src/App.js
@@ -31,6 +31,18 @@ const App = ({ pageLayout }) => {
             as="font"
             crossOrigin=""
           />
+        <meta charset="utf-8" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="As the workers at Medium, we want Actions to Matter too"
+        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <meta name="robots" content="nofollow" />
+        <link rel="manifest" href="/manifest.json" />
+        <title>Medium Workers Union (MWU)</title>
         </Head>
     <ChakraProvider>
       <LayoutDefault>
