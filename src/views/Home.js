46e3438a0ddc9fa4dce165462f8e5c0484ae422b
therebelrobot <github@therebelrobot.com>
2021-02-13 20:17:33 -0800

    removed cta until PII review can occur

--- src/views/Home.js
+++ src/views/Home.js
@@ -19,10 +19,18 @@ const Home = () => {
         className="illustration-section-02"
       />
       <FAQTiles />
-      <PressKit/>
-      {/* This stays commented out until we reach > 50% members submitted */}
+      <PressKit />
+      <div
+        style={{
+          width: "100%",
+          height: "1px",
+          borderBottom: "solid 1px #d97f76",
+        }}
+      />
+      {/* TODO: This stays commented out until we reach > 50% members submitted */}
       {/* <Testimonial topDivider /> */}
-      <Cta split />
+      {/* TODO: Commenting this out until a PII audit can be done */}
+      {/* <Cta split /> */}
     </>
   );
 };
